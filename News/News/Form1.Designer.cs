809f25e87bb40c194425e4eef5cce72c248526b8
RiccardoNasatti <riccardonasatti@gmail.com>
2023-02-28 18:16:47 +0100

    start

--- News/News/Form1.Designer.cs
+++ News/News/Form1.Designer.cs
@@ -28,47 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.title = new System.Windows.Forms.Label();
+            this.description = new System.Windows.Forms.Label();
+            this.content = new System.Windows.Forms.TextBox();
+            this.previous = new System.Windows.Forms.Button();
+            this.next = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(353, 77);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(407, 234);
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.Size = new System.Drawing.Size(816, 67);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "THE WALL STREET JOURNAL";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-8, -6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(818, 69);
+            this.panel1.TabIndex = 2;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.dateTimePicker1.Location = new System.Drawing.Point(300, 84);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.AutoSize = true;
+            this.panel2.Controls.Add(this.content);
+            this.panel2.Controls.Add(this.description);
+            this.panel2.Controls.Add(this.title);
+            this.panel2.Location = new System.Drawing.Point(12, 134);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(776, 304);
+            this.panel2.TabIndex = 4;
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Modern No. 20", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(3, 0);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(78, 34);
+            this.title.TabIndex = 0;
+            this.title.Text = "Title";
+            // 
+            // description
+            // 
+            this.description.AutoSize = true;
+            this.description.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.description.Location = new System.Drawing.Point(4, 34);
+            this.description.Name = "description";
+            this.description.Size = new System.Drawing.Size(128, 28);
+            this.description.TabIndex = 1;
+            this.description.Text = "Description";
+            // 
+            // content
+            // 
+            this.content.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.content.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.content.Location = new System.Drawing.Point(0, 78);
+            this.content.Margin = new System.Windows.Forms.Padding(4);
+            this.content.Multiline = true;
+            this.content.Name = "content";
+            this.content.Size = new System.Drawing.Size(755, 215);
+            this.content.TabIndex = 8;
+            // 
+            // previous
+            // 
+            this.previous.Location = new System.Drawing.Point(12, 83);
+            this.previous.Name = "previous";
+            this.previous.Size = new System.Drawing.Size(75, 23);
+            this.previous.TabIndex = 5;
+            this.previous.Text = "Previous";
+            this.previous.UseVisualStyleBackColor = true;
+            this.previous.Click += new System.EventHandler(this.previous_Click);
+            // 
+            // next
+            // 
+            this.next.Location = new System.Drawing.Point(713, 84);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(75, 23);
+            this.next.TabIndex = 6;
+            this.next.Text = "Next";
+            this.next.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.next);
+            this.Controls.Add(this.previous);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label description;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.TextBox content;
+        private System.Windows.Forms.Button previous;
+        private System.Windows.Forms.Button next;
     }
 }
 
--- News/News/Form1.cs
+++ News/News/Form1.cs
@@ -23,24 +23,38 @@ namespace News
         }
 
         static HttpClient client = new HttpClient();
+        Rootobject articolo = new Rootobject();
+        int i = 0;
 
-        private async void button1_Click(object sender, EventArgs e)
-        {
-            await get();
-        }
         async Task<Rootobject> get()
         {
-            Rootobject product = null;
             string url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=a6412302f797446fb5d0cee1c946e308";
             HttpResponseMessage response = await client.GetAsync(url);
             MessageBox.Show(response.ToString());
             if (response.IsSuccessStatusCode)
             {
-                product = await JsonSerializer.DeserializeAsync<Rootobject>(await response.Content.ReadAsStreamAsync());
-                MessageBox.Show(product.tostring());
-                label1.Text = product.tostring();
+                articolo = await JsonSerializer.DeserializeAsync<Rootobject>(await response.Content.ReadAsStreamAsync());
+                MessageBox.Show(articolo.tostring());
+                complete(i);
             }
-            return product;
+            return articolo;
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await get();
+        }
+
+        private void previous_Click(object sender, EventArgs e)
+        {
+            i++;
+            complete(i);
+        }
+        private void complete(int n)
+        {
+                title.Text = articolo.articles[n].title;
+                description.Text = articolo.articles[n].description;
+                content.Text = articolo.articles[n].content;
         }
     }
 }
